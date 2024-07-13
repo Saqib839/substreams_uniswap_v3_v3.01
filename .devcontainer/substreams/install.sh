6825b7813c3faf9b761a8233dbc3f832c7708624
Alexandre Bourget <alex@streamingfast.io>
2024-07-13 00:53:17 -0400

    Fix the `substreams` feature side loading

--- .devcontainer/substreams/install.sh
+++ .devcontainer/substreams/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-LINK=$(curl -s https://api.github.com/repos/streamingfast/substreams/releases/latest | awk "/download.url.*linux_$(uname -m)/ {print \$2}" | sed 's/"//g')
+LINK=$(curl -s https://api.github.com/repos/streamingfast/substreams/releases/latest | awk "/download.url.*linux_$(uname -m | sed s/aarch64/arm64/)/ {print \$2}" | sed 's/"//g')
+echo Downloading $LINK
 curl -L  $LINK  | tar zxf -
 mv substreams /usr/bin/substreams
